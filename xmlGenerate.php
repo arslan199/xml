e404efba19ff00c89a052a69147decb92fc39357
arslan199 <UnitySpace@yandex.ru>
2016-10-21 14:24:53 -0700

    Add files via upload

--- xmlGenerate.php
+++ xmlGenerate.php
@@ -9,27 +9,239 @@ class xmlGenerate
 
     }
 
-    public function xmlCreate()
+    public function ymlCreate($yml_array = array())
     {
-        $root = $this->dom->createElement("users"); // Создаём корневой элемент
-        $this->dom->appendChild($root);
-        $logins = array("User1", "User2", "User3"); // Логины пользователей
-        $passwords = array("Pass1", "Pass2", "Pass3"); // Пароли пользователей
-        for ($i = 0; $i < count($logins); $i++) {
-            $id = $i + 1; // id-пользователя
-            $user = $this->dom->createElement("user"); // Создаём узел "user"
-            $user->setAttribute("id", $id); // Устанавливаем атрибут "id" у узла "user"
-            $login = $this->dom->createElement("login", $logins[$i]); // Создаём узел "login" с текстом внутри
-            $password = $this->dom->createElement("password", $passwords[$i]); // Создаём узел "password" с текстом внутри
-            $user->appendChild($login); // Добавляем в узел "user" узел "login"
-            $user->appendChild($password);// Добавляем в узел "user" узел "password"
-            $root->appendChild($user); // Добавляем в корневой узел "users" узел "user"
-        }
-        $this->dom->save("users.xml"); // Сохраняем полученный XML-документ в файл
-    }
 
+        $yml = $this->dom->createElement('yml_catalog'); // создаем yml каталог
+        $yml->setAttribute('date', date('Y-m-d h:m'));
+        $this->dom->appendChild($yml);
+
+        $shop = $this->dom->createElement('shop'); // создаем магазин
+        $yml->appendChild($shop);
+
+        foreach ($yml_array as $first_lvl) // собераем данные 1 уровня вложенности из полученного массива
+        {
+            $teg = $this->dom->createElement($first_lvl['name'], $first_lvl['text']); // создаем тэг
+            $shop->appendChild($teg);
+
+            if($first_lvl['attributes'] !== null && is_array($first_lvl['attributes'])) // если атрибуты есть записываем
+            {
+                foreach ($first_lvl['attributes'] as $atr_key => $atr_value)
+                {
+                    $teg->setAttribute($atr_key, $atr_value);
+                }
+            }
+
+            if($first_lvl['inserted'] !== null && is_array($first_lvl['inserted'])) // если есть вложенные тэги то создаем
+            {
+                foreach ($first_lvl['inserted'] as $second_lvl)
+                {
+                    $teg_second = $this->dom->createElement($second_lvl['name'], $second_lvl['text']);
+                    $teg->appendChild($teg_second);
+
+                    if($second_lvl['attributes'] !== null && is_array($second_lvl['attributes'])) // добовляем атрибуты 2 уровню тэгам
+                    {
+                        foreach ($second_lvl['attributes'] as $atr)
+                        {
+                            $teg_second->setAttribute($atr['name'], $atr['value']);
+                        }
+                    }
+
+                    if($second_lvl['inserted'] !== null && is_array($second_lvl['inserted'])) // проверяем есть ли 3 уровень вложенности
+                    {
+                        foreach ($second_lvl['inserted'] as $third_lvl)
+                        {
+                            $teg_third = $this->dom->createElement($third_lvl['name'], $third_lvl['text']);
+                            $teg_second->appendChild($teg_third);
+
+                            if($third_lvl['attributes'] !== null && is_array($third_lvl['attributes'])) // задаем атрибуты 3 уровню
+                            {
+                                foreach ($third_lvl['attributes'] as $atr)
+                                {
+                                    $teg_third->setAttribute($atr['name'], $atr['value']);
+                                }
+                            }
+
+                            if($third_lvl['inserted'] !== null && is_array($third_lvl['inserted'])) // проверяем есть ли 4 уровень вложенности
+                            {
+                                foreach ($third_lvl['inserted'] as $four_lvl)
+                                {
+                                    $teg_four = $this->dom->createElement($four_lvl['name'], $four_lvl['text']);
+                                    $teg_third->appendChild($teg_four);
+
+                                    if($four_lvl['attributes'] !== null && is_array($four_lvl['attributes'])) // задаем атрибуты 4 уровню
+                                    {
+                                        foreach ($four_lvl['attributes'] as $atr)
+                                        {
+                                            $teg_four->setAttribute($atr['name'], $atr['value']);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+            $this->dom->save("yml2.xml");
+        }
+    }
 }
 
-$dom = new xmlGenerate();
+// массив который будем собирать в админке
+$yml_array = array(
+    array(
+        'name' => 'name',
+        'attributes' => null,
+        'text' => 'ABC',
+        'inserted' => null,
+    ),
+    array(
+        'name' => 'company',
+        'attributes' => null,
+        'text' => 'ABC inc.',
+        'inserted' => null,
+    ),
+    array(
+        'name' => 'url',
+        'attributes' => null,
+        'text' => 'ABC.com',
+        'inserted' => null,
+    ),
+    array(
+        'name' => 'currencies',
+        'attributes' => null,
+        'text' => null,
+        'inserted' => array(
+            array(
+                'name' => 'currency',
+                'attributes' => array(
+                    array(
+                        'name' => 'id',
+                        'value' => 'RUR',
+                    ),
+                    array(
+                        'name' => 'rate',
+                        'value' => '80',
+                    ),
+                ),
+                'text' => null,
+                'inserted' => null,
+            ),
+            array(
+                'name' => 'currency',
+                'attributes' => array(
+                    array(
+                        'name' => 'id',
+                        'value' => 'RUR',
+                    ),
+                    array(
+                        'name' => 'rate',
+                        'value' => '80',
+                    ),
+                ),
+                'text' => null,
+                'inserted' => null,
+            ),
+        ),
+    ),
+    array(
+        'name' => 'categories',
+        'attributes' => null,
+        'text' => null,
+        'inserted' => array(
+            array(
+                'name' => 'category',
+                'attributes' => array(
+                    array(
+                        'name' => 'id',
+                        'value' => '1234',
+                    ),
+                    array(
+                        'name' => 'parentId',
+                        'value' => '1278',
+                    ),
+                ),
+                'text' => 'Телевизоры',
+                'inserted' => null,
+            ),
+            array(
+                'name' => 'category',
+                'attributes' => array(
+                    array(
+                        'name' => 'id',
+                        'value' => '1244',
+                    ),
+                    array(
+                        'name' => 'parentId',
+                        'value' => '3278',
+                    ),
+                ),
+                'text' => 'Медиа-плееры',
+                'inserted' => null,
+            ),
+        ),
+    ),
+    array(
+        'name' => 'offers',
+        'attributes' => null,
+        'text' => null,
+        'inserted' => array(
+            array(
+                'name' => 'offer',
+                'attributes' => array(
+                    array(
+                        'name' => 'id',
+                        'value' => '1234',
+                    ),
+                    array(
+                        'name' => 'available',
+                        'value' => 'true',
+                    ),
+                    array(
+                        'name' => 'bid',
+                        'value' => '1278',
+                    ),
+                    array(
+                        'name' => 'cbid',
+                        'value' => '1278',
+                    ),
+                ),
+                'text' => null,
+                'inserted' => array(
+                    array(
+                        'name' => 'delivery-options',
+                        'attributes' => null,
+                        'text' => null,
+                        'inserted' => array(
+                            array(
+                                'name' => 'option',
+                                'attributes' => array(
+                                    array(
+                                        'name' => 'cost',
+                                        'value' => '1000',
+                                    ),
+                                    array(
+                                        'name' => 'days',
+                                        'value' => '1',
+                                    ),
+                                    array(
+                                        'name' => 'order-before',
+                                        'value' => '15',
+                                    ),
+                                ),
+                                'text' => null,
+                                'inserted' => null,
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+);
 
-$dom->xmlCreate();
+// вызываем скрипт создания юмл документа
+$dom = new xmlGenerate($yml_array);
+
+$dom->ymlCreate();
